ae74452e38c8fa17b71626f028394edf4f9e027a
Melissa2016 <32329679+Melissa2016@users.noreply.github.com>
2018-08-28 10:35:34 +0200

    first commit

---
+++ src/test/java/se/backend/groupred2/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package se.backend.groupred2.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    @Test
+    public void deActivate() {
+    }
+}
